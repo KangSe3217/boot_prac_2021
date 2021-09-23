18da2e32e95e65501daacb382c7f80f32bf160a8
KangSe3217 <32170048@dankook.ac.kr>
2021-09-23 18:01:25 +0900

    delete_method

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -11,16 +11,18 @@ import java.util.List;
 @Controller
 
 public class UsrArticleController {
+    //매개변수
     private int articlesLastId;
     private List<Article> articles;
 
+    //생성자
     public UsrArticleController(){
         articlesLastId = 0;
         articles = new ArrayList<>();
 
         makeTestData();
     }
-
+    //서비스 메소드 시작
     private void makeTestData() {
         for(int i = 1 ; i<=10 ; i++){
             int id = articlesLastId + 1;
@@ -37,17 +39,49 @@ public class UsrArticleController {
         articlesLastId = id;
         return article;
     }
+    private Article getArticle(int id) {
+        for (Article article : articles) {
+            if(article.getId() == id){
+                return article;
+            }
+        }
+        return null;
+    }
+    private void deleteArticle(int id) {
+        Article article = getArticle(id);
 
+        articles.remove(article);
+    }
+
+    // 서비스 메소드 종료
+
+    //액션 메서드 시작
     @RequestMapping("/usr/article/doAdd")
     @ResponseBody
     public Article doAdd(String title,String body){
         Article article = writeArticle(title,body);
         return article;
     }
+
+    @RequestMapping("/usr/article/doDelete")
+    @ResponseBody
+    public String doDelete(int id){
+        Article article = getArticle(id);
+        if(article == null){
+            return id  + "번 게시물이 존재하지 않습니다.";
+        }
+        deleteArticle(id);
+        return id + "번 게시물을 삭제 하였습니다.";
+    }
+
+    // 액션 메서드 종료
+
     @RequestMapping("/usr/article/getArticles")
     @ResponseBody
     public List<Article> getArticles() {
         return articles;
     }
+
+
 
 }
